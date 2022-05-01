0e1a36c51fdabc792b18788ae62dd61731c260b8
AndyLow14 <andyjuncehgnlow@gmail.com>
2022-05-01 21:43:01 +1000

    Added a button to save quote to archive.txt

--- PyQuote.py
+++ PyQuote.py
@@ -1,6 +1,6 @@
 # Obtain Inspirational Quotes by using Python, "https://quotes.rest/qod"
 from concurrent.futures import thread
-from ctypes import sizeof
+from ctypes import alignment, sizeof
 from textwrap import wrap
 import requests
 import tkinter as tk
@@ -10,6 +10,7 @@ from threading import Thread
 api = "http://api.quotable.io/random"
 quotes = []
 i_quote = 0
+curr_quote = ""
 
 # Tkinter Window
 window = tk.Tk()
@@ -36,16 +37,32 @@ def load_quotes():
 
 load_quotes()
 
+def save():
+    global curr_quote
+
+    f = open("archive.txt", "a")
+    f.write(curr_quote + "\n\n")
+    f.close()
+    # Disable button after one click
+    save_button.config(state="disabled")
+
 # UI
 quote_label = tk.Label(window, font=("Minecraftia", 18), background="black", foreground="white", wraplength=590)
 quote_label.pack(padx=35, pady=10)
 quote_label.place(relx=.5, rely=.5, anchor="c")
+save_button = tk.Button(window, text="SAVE", font=("Minecraftia", 12), background="black", foreground="white", activebackground="black", relief="solid", anchor="c", command=save)
+save_button.tkraise()
+save_button.pack(side="top", anchor="ne", padx=10, pady=10)
 
 def get_random_quote():
     global quote_label
     global quotes
+    global curr_quote
 
-    quote_label.config(text=quotes.pop(0))
+    curr_quote = quotes.pop(0)
+    quote_label.config(text=curr_quote)
+    # Re-enable button
+    save_button.config(state="normal")
 
     if len(quotes) == 1:
         thread = Thread(target=load_quotes)
